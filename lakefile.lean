83e1081dea01822df2519a821ed856c3fdac23ee
Paul <yupengzh@umich.edu>
2025-02-22 15:25:56 -0600

    bump

--- lakefile.lean
+++ lakefile.lean
@@ -5,7 +5,10 @@ package «lean4-example» {
   -- add package configuration options here
 }
 
+
 @[default_target]
 lean_lib «Lean4Example» {
   -- add library configuration options here
 }
+
+require mathlib from git "https://github.com/leanprover-community/mathlib4" @ "v4.15.0"
